2ef9c3b42468c7524d704de0e9db4ae699762588
pholmes7 <pqholmes@gmail.com>
2026-09-20 22:24:22 -0500

    Test end-to-end GitOps deployment

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World! I'm a real smacker"
+    return "Hello, World! We are Live like channel 5"
 
 
 if __name__ == "__main__":
